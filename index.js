38cc2b0dd8f2c3326ead46678af94f4a4bf3a701
LearnCoding2023 <tyson.fi89@gmail.com>
2023-08-23 12:32:21 +0530

    db connecion with POST

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const { getDataFromDb } = require('./mongo-service');
+const { getDataFromDb, createDocument  } = require('./mongo-service');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/health', (req, res) => {
     res.status(201).send('SUCCESS');
@@ -10,6 +14,16 @@ app.get('/health', (req, res) => {
 app.get('/mongodb/query', async (req, res) => {
     const response = await getDataFromDb();
     res.status(201).send(response);
+});
+
+app.post('/mongodb/create', (req, res) => {
+    let {
+        name,
+        age,
+        phone
+    } = req.body;
+    createDocument(name, age, phone)
+    res.status(200).json({message: 'Success'});
 })
 
 app.listen(3000);
--- mongo-service.js
+++ mongo-service.js
@@ -1,16 +1,23 @@
 const { MongoClient } = require('mongodb')
-
 const url = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.5"
 const client = new MongoClient(url)
-const database = 'mongodbPlaygroundDB'
 
-async function getDataFromDb() {
+const getDataFromDb = async () => {
     const conn = await client.connect();
-    const db = conn.db(database);
-    const collection = db.collection('Test');
+    const db = conn.db('mongodbVSCodePlaygroundDB');
+    const collection = db.collection('employee');
     const output = await collection.find({}).toArray();
     console.log(output)
     return output;
 }
 
-module.exports = { getDataFromDb }
+async function createDocument(incomingName, incomingAge, incomingPhone) {
+    const conn = await client.connect();
+    const db = conn.db('mongodbVSCodePlaygroundDB');
+    const collection = db.collection('employee');
+    const output = await collection.insertOne({name: incomingName, age: incomingAge, phone: incomingPhone})
+    console.log(output);
+    return output;
+}
+
+module.exports = { getDataFromDb, createDocument }
