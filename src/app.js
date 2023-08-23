994d4cdc7e35cfa1acd085d0dc1a06dce28571f3
LearnCoding2023 <tyson.fi89@gmail.com>
2023-08-23 13:39:25 +0530

    One more update to make it better

--- src/app.js
+++ src/app.js
@@ -5,13 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/employee', require('./src/routes/index'));
+app.use('/api/employee', require('./routes/index'));
 
 app.listen(3000, () => {
     console.log(`Server running in port 3000`)
 });
-
-function logger(req, res, next) {
-    console.log('logging it')
-    next()
-}
