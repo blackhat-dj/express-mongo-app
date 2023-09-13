4d9f2c703bf0ae6df0671127539b1057635ec1f3
Prince D <137522380+blackhat-dj@users.noreply.github.com>
2023-09-13 12:15:51 +0530

    Updated Code

--- source/routes/index.js
+++ source/routes/index.js
@@ -7,10 +7,8 @@ module.exports = app => {
     router.post('/post', cars.create);
     router.get('/get', cars.get);
     router.post('/purchase', cars.findOne);
-   // router.get('/purchase', cars.getData);
-   // router.post('/purchase',purchase.addData)
-     router.get('/query',cars.updatedData)
-     router.get('/purchase', cars.purchaseData);
+    router.get('/query',cars.updatedData)
+    router.get('/purchase', cars.purchaseData);
 
 
     app.use('/api/car', router);
